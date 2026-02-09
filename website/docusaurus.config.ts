334e2500f980dd54de55122461187e0bdad2c804
Anton Shiryaev <med.phisiker@gmail.com>
2026-02-10 01:39:35 +1000

    design: set logo variant 2

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -7,7 +7,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'Hyper Stack Manager',
   tagline: 'Environment Hypervisor for Modern Hybrid Stacks',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/logo-v2.svg',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   // future: {
